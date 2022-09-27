99517984fbcd9c5bfb12de6e1cb72e836fe6e989
Letícia Vieira <lvieira268@gmail.com>
2022-09-27 20:28:44 -0300

    feat: create model item
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

---
+++ lib/models/item.dart
@@ -0,0 +1,18 @@
+class Item {
+  String title = '';
+  bool done = false;
+
+  Item({this.title = '', this.done = false});
+
+  Item.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    done = json['done'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['title'] = title;
+    data['done'] = done;
+    return data;
+  }
+}

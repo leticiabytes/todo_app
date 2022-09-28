79d3dba48b9114242d41d809e97378c6b6450f52
Letícia Vieira <lvieira268@gmail.com>
2022-09-28 18:07:50 -0300

    feat: create consts with colors of the app
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xffda4040);
+const Color tdBlue = Color(0xff5f52ee);
+
+const Color tdBlack = Color(0xff3a3a3a);
+const Color tdGrey = Color(0xff717171);
+
+const Color tdBGColor = Color(0xffeeeff5);

e1a8568fb1b3dd0e57c7d96c739c92f0ecfa3ad6
Letícia Vieira <lvieira268@gmail.com>
2022-09-28 18:08:35 -0300

    feat: create style of AppBar
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

---
+++ lib/screens/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: tdBGColor, appBar: _buildAppBar());
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      leadingWidth: 80,
+      elevation: 0,
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        const Icon(
+          Icons.menu,
+          color: tdBlack,
+          size: 30,
+        ),
+        SizedBox(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset('assets/images/avatar.jpeg'),
+          ),
+        ),
+      ]),
+    );
+  }
+}

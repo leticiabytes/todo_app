373479e50a9be6a9a81f9a650a06ea252a65d6bd
Letícia Vieira <lvieira268@gmail.com>
2022-09-28 18:08:19 -0300

    feat: update main with new widget appBar
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-import 'models/item.dart';
+import 'screens/home.dart';
 
 void main() {
   runApp(const App());
@@ -12,43 +13,15 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'To-Do App',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  var items = <Item>[];
-
-  HomePage({super.key}) {
-    items = [];
-    items.add(Item(title: 'item 1', done: false));
-    items.add(Item(title: 'item 2', done: false));
-  }
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('To-Do App'),
-      ),
-      body: ListView.builder(
-        itemCount: widget.items.length,
-        itemBuilder: (BuildContext ctxt, int index) {
-          return Text(widget.items[index].title);
-        },
-      ),
+      home: Home(),
     );
   }
 }

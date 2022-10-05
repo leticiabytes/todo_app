f72f203108eee5222b7181a7c93b6fb18e7ef688
Letícia Vieira <lvieira268@gmail.com>
2022-10-05 18:54:40 -0300

    feat: add textField and elevationButton to home screen
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,30 +13,70 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-                child: ListView(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 50, bottom: 20),
-                  child: const Text(
-                    'All ToDos',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+      body: Stack(children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          child: Column(
+            children: [
+              searchBox(),
+              Expanded(
+                  child: ListView(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 50, bottom: 20),
+                    child: const Text(
+                      'All ToDos',
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                    ),
                   ),
-                ),
-                for (ToDo todo in todosList)
-                  TodoItem(
-                    todo: todo,
-                  ),
-              ],
-            ))
-          ],
+                  for (ToDo todo in todosList)
+                    TodoItem(
+                      todo: todo,
+                    ),
+                ],
+              ))
+            ],
+          ),
         ),
-      ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Row(children: [
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0, 0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0)
+                    ],
+                    borderRadius: BorderRadius.circular(10)),
+                child: const TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Add item', border: InputBorder.none),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(bottom: 20, right: 20),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: tdBlue,
+                    minimumSize: const Size(60, 60),
+                    elevation: 10),
+                child: const Text('+', style: TextStyle(fontSize: 40)),
+              ),
+            )
+          ]),
+        )
+      ]),
     );
   }
 
